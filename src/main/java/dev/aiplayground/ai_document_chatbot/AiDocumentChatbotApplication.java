3cacd4265300b33dbee27b803b7ff5163876de6d
minhadevcode <ajgm307910@gmail.com>
2026-08-11 22:36:17 +0900

    변경

---
+++ src/main/java/dev/aiplayground/ai_document_chatbot/AiDocumentChatbotApplication.java
@@ -0,0 +1,13 @@
+package dev.aiplayground.ai_document_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocumentChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDocumentChatbotApplication.class, args);
+	}
+
+}
